803ae60a91bb0a6b0803bc7c5650cc7a90830a9b
Maarten de Vries <maarten@de-vri.es>
2016-11-07 21:44:29 +0100

    Use new fancy widgets.

--- qtile/config.py
+++ qtile/config.py
@@ -1,9 +1,13 @@
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
+from colorsys import rgb_to_hls, hls_to_rgb
 
 import os
 import subprocess
+
+from flexible_group_box import FlexibleGroupBox
+from multi_text_box import MultiTextBox
 
 mod = "mod4"
 
@@ -93,54 +97,114 @@ layouts = [
 	layout.Max(      name="Full",    **layout_color),
 ]
 
-# Define GroupBox settings
-group_settings = dict(
-	borderwidth                = 1,
-	highlight_method           = "block",
-	inactive                   = "#737373",
-	this_current_screen_border = "#3492B3",
-	other_screen_border        = "#00171F",
-	disable_drag               = True,
-)
+white = (0, 1, 0)
+black = (0, 0, 0)
 
-# Define widget settings
-widget_defaults = dict(
-	font     = "xft:DejaVu Sans Mono",
-	fontsize = 11,
-	padding  = 0,
-)
+def toQtileColor(hls):
+	r, g, b = hls_to_rgb(*hls)
+	return r * 255, g * 255, b * 255
 
-def get_bar():
+def screenColor(screen):
+	if screen == 0: return 120 / 360.0, 0.5, 0.5
+	if screen == 1: return  30 / 360.0, 0.5, 0.5
+	if screen == 2: return 210 / 360.0, 0.5, 0.5
+	return                 270 / 360.0, 0.5, 0.5
+
+def lsMultiply(hls, l, s):
+	return hls[0], hls[1] * l, hls[2] * s
+
+def background(hls):
+	return lsMultiply(hls, 0.4, 0.7)
+
+def fade(hls):
+	return lsMultiply(hls, 0.7, 0.8)
+
+def highlight(hls):
+	return lsMultiply(hls, 1.0, 1.5)
+
+def groupColors(screen, focus, windows, urgents):
+	if urgents:           return white,       (0, 0.6, 0)
+	if screen and focus:  return white,       screenColor(screen.index)
+	if screen:            return fade(white), fade(screenColor(screen.index))
+	if windows and focus: return black,       (0.0, 0.6, 0.0)
+	if windows:           return black,       fade((0.0, 0.6, 0.0))
+	return (0.0, 0.7, 0.0), (0.0, 0.2, 0.0)
+
+def makeGroupFormatter(screen):
+	def format(group, qtile):
+		focus = qtile.currentScreen.index == screen
+		urgents = filter(lambda x: x.urgent, group.windows)
+		fg, bg = groupColors(group.screen, focus, len(group.windows), 0)
+		return dict(
+			text      = ' {} '.format(group.name),
+			fg_colour = toQtileColor(fg),
+			bg_colour = toQtileColor(bg),
+		)
+	return format
+
+def makeLayoutFormatter(screen):
+	def format(widget, qtile):
+		focus = qtile.currentScreen.index == screen
+		group = qtile.screens[screen].group
+		text  = group.layout.name if group else ''
+		return [dict(
+			text      = ' {} '.format(text),
+			fg_colour = '#ffaa00' if focus else '#b0b0b0',
+			bg_colour = toQtileColor(background(screenColor(screen))),
+		)]
+	return format
+
+def makeTitleFormatter(screen):
+	def format(widget, qtile):
+		focus  = qtile.currentScreen.index == screen
+		group  = qtile.screens[screen].group
+		window = group.currentWindow if group else None
+		text   = window.name if window and window.name else ''
+		return [dict(
+			text      = ' {} '.format(text.encode('utf-8')),
+			fg_colour = toQtileColor(highlight(screenColor(screen))) if focus else '#bbbbbb',
+		)]
+	return format
+
+update_hooks = [
+	hook.subscribe.changegroup,
+	hook.subscribe.float_change,
+	hook.subscribe.focus_change,
+	hook.subscribe.layout_change,
+	hook.subscribe.window_name_change,
+]
+
+def get_bar(screen):
 	return bar.Bar(
 		[
 			# Groups
-			widget.GroupBox(**group_settings),
+			FlexibleGroupBox(makeGroupFormatter(screen), font_size=12),
 			widget.Spacer(length=8),
 
 			# Current layout
-			widget.CurrentLayout(),
+			MultiTextBox(formatter=makeLayoutFormatter(screen), hooks=update_hooks, font_size=12),
 			widget.Spacer(length=8),
 
 			# Current window
-			widget.WindowName(),
-			widget.Spacer(length=8),
+			MultiTextBox(formatter=makeTitleFormatter(screen), hooks=update_hooks, font_size=12),
+			widget.Spacer(length=bar.STRETCH),
 
 			# System tray
 			widget.Systray(),
 			widget.Spacer(length=8),
 
 			# Clock
-			widget.Clock(format='%a %b %d %H:%M:%S'),
+			widget.Clock(format='%a %d %b %H:%M:%S', font='xft:monospace', fontsize=12, foreground='#ffaa00'),
 			widget.Spacer(length=8),
 		],
 		16,
-		background=["#00394d", "#104E63"],
+		background = toQtileColor(background(screenColor(screen))),
 	)
 
 # Define bars on screens
 screens = [
-	Screen(top=get_bar()),
-	Screen(top=get_bar()),
+	Screen(top=get_bar(0)),
+	Screen(top=get_bar(1)),
 ]
 
 # Drag floating layouts.
