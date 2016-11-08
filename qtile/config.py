ad5056387d0699d913873dec6fe5433743500793
Maarten de Vries <maarten@de-vri.es>
2016-11-08 18:22:44 +0100

    Make left-click drag float and move window.

--- qtile/config.py
+++ qtile/config.py
@@ -215,9 +215,8 @@ def makeScreensConfig(qtile):
 
 # Drag floating layouts.
 mouse = [
-	Drag([mod], "Button1", lazy.window.set_position(),          start=lazy.window.get_position()),
+	Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
 	Drag([mod], "Button3", lazy.window.set_size_floating(),     start=lazy.window.get_size()),
-	Drag([mod], "Button2", lazy.window.set_position_floating(), start=lazy.window.get_position()),
 	#Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
