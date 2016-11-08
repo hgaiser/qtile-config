5f625fb0d2902fd1851d61255004780e1410643d
Maarten de Vries <maarten@de-vri.es>
2016-11-08 15:18:13 +0100

    Reformat mouse bindings.

--- qtile/config.py
+++ qtile/config.py
@@ -210,12 +210,9 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-	Drag([mod], "Button1", lazy.window.set_position(),
-		start=lazy.window.get_position()),
-	Drag([mod], "Button3", lazy.window.set_size_floating(),
-		start=lazy.window.get_size()),
-	Drag([mod], "Button2", lazy.window.set_position_floating(),
-		start=lazy.window.get_position()),
+	Drag([mod], "Button1", lazy.window.set_position(),          start=lazy.window.get_position()),
+	Drag([mod], "Button3", lazy.window.set_size_floating(),     start=lazy.window.get_size()),
+	Drag([mod], "Button2", lazy.window.set_position_floating(), start=lazy.window.get_position()),
 	#Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
