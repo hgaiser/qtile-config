610946a0d321507e24665a832cb17a139e5d19b4
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2016-11-11 11:00:15 +0100

    Fix searching for battery name in python.

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import datetime
 import os
 
 def find_battery_name(search_path="/sys/class/power_supply/"):
-	batteries = [b for b in os.listdir(search_path) if b.beginswith("BAT")]
+	batteries = [b for b in os.listdir(search_path) if b.startswith("BAT")]
 	if len(batteries) == 0:
 		return None
 	else:
